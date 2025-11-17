7a406dcd4d6a34b3c51ad3c4928480e74bc2f110
khanazlan <azlankhan7266@gmail.com>
2025-11-18 01:27:50 +0530

    First commit

--- aws/local_state/main.tf
+++ aws/local_state/main.tf
@@ -1,1 +1,8 @@
+provider "aws" {
+    region = "eu-nort-1" #set your desired AWS region
+}
 
+resource "aws_instance" "example" {
+    ami = "ami-0fa91bc90632c73c9" #Specify an appropriate AMI ID
+    instance_type = "t2.micro"
+}
